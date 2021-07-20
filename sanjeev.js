3bfeffc5cf7cab8c0a84c250ce6d3a092da2b009
sanjuosnyy <sanjusonyy@gmail.com>
2021-07-20 22:58:04 +0530

    done

--- sanjeev.js
+++ sanjeev.js
@@ -1,13 +1,13 @@
-// console.log('hello world');
+console.log('hello world');
 // let x = 5 + 5;
 // let y = "5" + 5;
 // let z = "Hello" + 5;
 // console.log('x+y');
 
 // find sqare root
-ver a = Math.sqrt(9);
-ver b = Math.sqrt(16);
-ver c = Math.sqrt(32);
-ver d = Math.sqrt(64);
-ver e = Math.sqrt(324);
-console.log(a,b,c,d,e)
+// ver a = Math.sqrt(9);
+// ver b = Math.sqrt(16);
+// ver c = Math.sqrt(32);
+// ver d = Math.sqrt(64);
+// ver e = Math.sqrt(324);
+// console.log(a,b,c,d,e)
